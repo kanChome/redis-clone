294385b7667c831104229881fcb076efe3f8acf6
kanChome <hellbird1998@gmail.com>
2025-10-03 01:42:05 +0900

    feat: pingの動作確認

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -29,12 +30,31 @@ func main() {
 			fmt.Println(err)
 			return
 		}
-		fmt.Println(value)
 
-		_ = value
+		if value.typ != "array" {
+			fmt.Println("Invalid request, excepted array")
+			continue
+		}
+
+		if len(value.array) == 0 {
+			fmt.Println("Invalid request, excepted array length > 0")
+			continue
+		}
+
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
 
 		writer := NewWriter(connect)
-		writer.Write(Value{typ: "string", str: "OK"})
+
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			writer.Write(Value{typ: "string", str: ""})
+			continue
+		}
+
+		result := handler(args)
+		writer.Write(result)
 
 		// テストのためrequestを無視し一時的にsOKを返す
 		connect.Write([]byte("+OK\r\n"))
